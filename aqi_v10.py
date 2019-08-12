2a2009158b11840019bdc867dc72b606b79c4be0
zqc <897223005@qq.com>
2019-08-12 11:17:59 +0800

    2090812-02

--- aqi_v10.py
+++ aqi_v10.py
@@ -4,12 +4,7 @@
     version:10.0 数据清洗
 """
 
-##导包
-import requests
-from bs4 import BeautifulSoup
-import json
-import csv
-import os
+##导入数据分析包
 import pandas as pd
 import matplotlib.pyplot as plt
 
